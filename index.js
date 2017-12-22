8c5bc9dbfe0f48869cfc21fb37717404af41822d
JONATHAN EASTIN <j.d.east@icloud.com>
2017-12-22 01:27:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function theBeatlesPlay(musicians, instruments) {
+  var newArray = [];
+  for (var i = 0; i < musicians.length; i++) {
+    newArray.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return newArray;
+}
+
+function johnLennonFacts(array) {
+  var facts = [];
+  var i = 0;
+  while (i < array.length) {
+    facts.push(`${array[i]}!!!`);
+  }
+}
